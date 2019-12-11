c81933a07697d01d9325bf1ed38c927dce8ff149
unknown <newmohib@gmail.com>
2019-12-11 16:59:29 +0600

    added docker multiple container connect nodejs api

---
+++ Docker-app-2/app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors')
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.use(cors())
+app.use(logger('dev'));
+//app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+app.listen(4000,(req,res)=>{
+    console.log("server run in Port:",4000);
+  });
+
+module.exports = app;
---
+++ Docker-app/app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+app.use(logger('dev'));
+//app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(3000,(req,res)=>{
+    console.log("server run in Port:",3000);
+  });
+
+module.exports = app;
